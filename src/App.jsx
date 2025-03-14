667ebe555041d798156580a5e42ada7b9ba35cfa
lalala0095 <torrefiellemuel@gmail.com>
2025-03-14 22:35:16 +0800

    EDIT removed dynamic gTag

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 import TechStackShowcase from "./components/TechStacks";
-import TagManager from "react-gtm-module";
+// import TagManager from "react-gtm-module";
 import { useEffect } from "react";
 
-const tagManagerArgs = {
-  gtmId: "G-B2PCN0K3JB",
-};
+// const tagManagerArgs = {
+//   gtmId: "G-B2PCN0K3JB",
+// };
 
 function App() {
-  useEffect(() => {
-    TagManager.initialize(tagManagerArgs);
-  }, []);
+  // useEffect(() => {
+  //   TagManager.initialize(tagManagerArgs);
+  // }, []);
   return (
     <div>
       <Header />
